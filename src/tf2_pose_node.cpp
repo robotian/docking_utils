e00125f02035ce1296c66464409ebb1bcef793aa
robotian <robotian@gmail.com>
2026-08-30 21:09:41 +0000

    one detected_pose at a time

--- src/tf2_pose_node.cpp
+++ src/tf2_pose_node.cpp
@@ -17,7 +17,9 @@ Tf2PoseNode::Tf2PoseNode(const rclcpp::NodeOptions & options)
 
   // Init TF2
   tf_buffer_ = std::make_unique<tf2_ros::Buffer>(this->get_clock());
-  tf_listener_ = std::make_shared<tf2_ros::TransformListener>(*tf_buffer_);
+  // tf_listener_ = std::make_shared<tf2_ros::TransformListener>(*tf_buffer_);
+  tf_listener_ = std::make_shared<tf2_ros::TransformListener>(*tf_buffer_, this);
+
 
   // Init Publisher
   pose_pub_ = this->create_publisher<geometry_msgs::msg::PoseStamped>(output_pose_topic_, 10);
@@ -30,13 +32,23 @@ Tf2PoseNode::Tf2PoseNode(const rclcpp::NodeOptions & options)
 void Tf2PoseNode::timer_callback()
 {
   try {
-    // Note: Use lookupTransform (camelCase) in C++
+    // 1. Get the latest available transform
     auto transform = tf_buffer_->lookupTransform(
       output_frame_, 
       target_frame_, 
-      tf2::TimePointZero, 
-      tf2::durationFromSec(timeout_s_)); // Note: durationFromSec (camelCase)
+      tf2::TimePointZero);
 
+    // 2. Check if the transform is stale (i.e. the TF publisher stopped)
+    rclcpp::Time tf_time(transform.header.stamp);
+    rclcpp::Duration age = this->now() - tf_time;
+
+    if (age.seconds() > timeout_s_) {
+      RCLCPP_DEBUG_THROTTLE(this->get_logger(), *this->get_clock(), 5000, 
+                           "TF is stale (age: %.2fs). Waiting for fresh data...", age.seconds());
+      return; // Exit the callback without publishing
+    }
+
+    // 3. Build and publish the pose
     geometry_msgs::msg::PoseStamped pose_msg;
     pose_msg.header = transform.header;
     pose_msg.pose.position.x = transform.transform.translation.x;
@@ -47,10 +59,43 @@ void Tf2PoseNode::timer_callback()
     pose_pub_->publish(pose_msg);
   }
   catch (const tf2::TransformException & ex) {
-    RCLCPP_WARN_THROTTLE(this->get_logger(), *this->get_clock(), 5000, 
-                         "TF Lookup Failed: %s", ex.what());
+    // Only print warnings if the node has been running for more than 3 seconds
+    if (this->now().nanoseconds() > 3e9) { 
+      RCLCPP_WARN_THROTTLE(this->get_logger(), *this->get_clock(), 5000, 
+                           "TF Lookup Failed: %s", ex.what());
+    }
   }
 }
+
+// void Tf2PoseNode::timer_callback()
+// {
+//   try {
+//     // Note: Use lookupTransform (camelCase) in C++
+//     // auto transform = tf_buffer_->lookupTransform(
+//     //   output_frame_, 
+//     //   target_frame_, 
+//     //   tf2::TimePointZero, 
+//     //   tf2::durationFromSec(timeout_s_)); // Note: durationFromSec (camelCase)
+    
+//     auto transform = tf_buffer_->lookupTransform(
+//       output_frame_, 
+//       target_frame_, 
+//       tf2::TimePointZero);
+
+//     geometry_msgs::msg::PoseStamped pose_msg;
+//     pose_msg.header = transform.header;
+//     pose_msg.pose.position.x = transform.transform.translation.x;
+//     pose_msg.pose.position.y = transform.transform.translation.y;
+//     pose_msg.pose.position.z = transform.transform.translation.z;
+//     pose_msg.pose.orientation = transform.transform.rotation;
+
+//     pose_pub_->publish(pose_msg);
+//   }
+//   catch (const tf2::TransformException & ex) {
+//     RCLCPP_INFO_THROTTLE(this->get_logger(), *this->get_clock(), 5000, 
+//                          "TF Lookup Failed: %s", ex.what());
+//   }
+// }
 } // namespace docking_utils
 
 #include "rclcpp_components/register_node_macro.hpp"
